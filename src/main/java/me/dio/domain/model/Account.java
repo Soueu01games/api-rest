61df588b178c723bf53b6e354a37af1a7576ceb9
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-11-11 17:59:37 -0300

    parei no 18:33-ORM

--- src/main/java/me/dio/domain/model/Account.java
+++ src/main/java/me/dio/domain/model/Account.java
@@ -1,10 +1,25 @@
 package me.dio.domain.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity(name = "tb_account")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique =true)
     private String number;
+
     private String agency;
+
+    @Column(name = "additional_limit" , scale = 13 ,precision = 2)
     private double balance;
+
+    @Column(name = "additional_limit" , scale = 13 ,precision = 2)
     private double limit;
 
     public Long getId() {return id;}
--- src/main/java/me/dio/domain/model/Card.java
+++ src/main/java/me/dio/domain/model/Card.java
@@ -1,19 +1,29 @@
 package me.dio.domain.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity(name = "tb_card")
 public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique =  true)
     private String number;
+
+    @Column(name = "available_limit", scale = 13 ,precision = 2)
     private double limit;
 
-    // Getter e Setter para id
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    // Getter e Setter para number
+
     public String getNumber() { return number; }
     public void setNumber(String number) { this.number = number; }
 
-    // Getter e Setter para limit
     public double getLimit() { return limit; }
     public void setLimit(double limit) { this.limit = limit; }
 }
