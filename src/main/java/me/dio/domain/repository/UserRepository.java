2459fbc2ba98c42ff04dfc28fd1109f0ed0c4429
Soueu01games <emanuelahelenamayeda@gmail.com>
2024-11-12 16:47:01 -0300

    desisti de tentar mudar a variavel de ambiente

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package me.dio.domain.repository;
 import me.dio.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 public interface UserRepository extends JpaRepository<User, Long>{
     
